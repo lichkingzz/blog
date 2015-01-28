075c8157ea08aa10ed98d44f825c20c9693101e5
lichkingzz <lichking.zz@gmail.com>
2015-01-28 16:03:29 +0800

    Signed-off-by: lichkingzz <lichking.zz@gmail.com>

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,44 +1,46 @@
 var sectionHeight = function() {
-  var total    = $(window).height(),
-      $section = $('section').css('height','auto');
+  var total = $(window).height(),
+    $section = $('section').css('height', 'auto'),
+    wmargin = 190;
 
   if ($section.outerHeight(true) < total) {
-    var margin = $section.outerHeight(true) - $section.height();
-    $section.height(total - margin - 20);
+    $section.height((total - wmargin) * 0.6);
   } else {
-    $section.css('height','auto');
+    $section.css('height', 'auto');
   }
-}
+};
 
 $(window).resize(sectionHeight);
 
-$(document).ready(function(){
-  $("section h1, section h2").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+$(document).ready(function() {
+  $("section h1, section h2").each(function() {
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '') + "'>" + $(this).text() + "</a></li>");
+    $(this).attr("id", $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, ''));
     $("nav ul li:first-child a").parent().addClass("active");
   });
-  
+
   $("nav ul li").on("click", "a", function(event) {
     var position = $($(this).attr("href")).offset().top - 190;
-    $("html, body").animate({scrollTop: position}, 400);
+    $("html, body").animate({
+      scrollTop: position
+    }, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
-    event.preventDefault();    
+    event.preventDefault();
   });
-  
+
   sectionHeight();
-  
+
   $('img').load(sectionHeight);
 });
 
 fixScale = function(doc) {
 
   var addEvent = 'addEventListener',
-      type = 'gesturestart',
-      qsa = 'querySelectorAll',
-      scales = [1, 1],
-      meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+    type = 'gesturestart',
+    qsa = 'querySelectorAll',
+    scales = [1, 1],
+    meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
 
   function fix() {
     meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
@@ -47,7 +49,7 @@ fixScale = function(doc) {
 
   if ((meta = meta[meta.length - 1]) && addEvent in doc) {
     fix();
-    scales = [.25, 1.6];
+    scales = [0.25, 1.6];
     doc[addEvent](type, fix, true);
   }
 };
